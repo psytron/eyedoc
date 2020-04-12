741d53bbe83ba058e840f4263a5547db35d08a8b
psytron <m@psytron.com>
2020-04-12 06:49:05 +0100

    format event log with ip

--- swarmwatch.py
+++ swarmwatch.py
@@ -5,18 +5,26 @@ import docker
 import time
 
 client = docker.from_env()
-
+clientAPI = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 while True:
-    print('\nCluster Update: ')
     for container in client.containers.list():
         con = client.containers.get( container.id )
         try:
+
+            inspection_obj = clientAPI.inspect_container( container.id )
+            container_networks = inspection_obj['NetworkSettings']['Networks']
+            for n in container_networks:
+                network_obj = container_networks[n]
+                IP_Address = network_obj['IPAddress']
             print( '' )
-            print( '          container: ', container.id[:6] )
-            print( ' wants traffic from: ', con.labels['com.roo.domain'] )
-            print( '    is listening on: ', [ x for x in con.ports ] ,'\n')
+            print( '          container: ', container.id[:5] )
+            print( '     expects domain: ', con.labels['com.roo.domain'] )
+            print( '           on PORTS: ', [ x for x in con.ports ] )
+            print( '         IP Address: ', IP_Address ,'\n')
+
         except Exception as e:
+            print( '     expects domain: ','NONE ( NOT PUBLIC ) ')
             pass
 
     time.sleep(5)
