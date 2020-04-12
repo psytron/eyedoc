d8961834601c623099e4388020c0d969bb95f6f8
psytron <m@psytron.com>
2020-04-12 22:16:23 +0100

    update system d bin

--- install.py
+++ install.py
@@ -5,6 +5,13 @@
 import os
 import shutil
 
+# TODO: PARAMETERIZE SystemD file
+
+# INSTALL SYSTEMD SERVICE
 target_path = '/etc/systemd/eyedoc.service'
 shutil.copyfile('systemd/eyedoc.service', target_path )
+os.chmod( target_path , 0o664)
+
+# COPY DAEMON BINARY
+shutil.copyfile('systemd/eyedoc', '/usr/local/eyedoc' )
 os.chmod( target_path , 0o664)
